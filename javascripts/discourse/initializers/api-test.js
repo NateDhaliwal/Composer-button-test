21ba480b7b40fbd575dd6bc0efeb6e24e1ca2f1e
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-01-15 19:10:10 +0800

    Create api-test.js

---
+++ javascripts/discourse/initializers/api-test.js
@@ -0,0 +1,19 @@
+import { apiInitializer } from 'discourse/lib/api';
+
+export default apiInitializer("0.11.1", (api) => {
+  api.addComposerToolbarPopupMenuOption({
+      action: (toolbarEvent) => {
+        toolbarEvent.applySurround("**", "**");
+      },
+      icon: 'far-bold',
+      label: 'composer.bold_some_text',
+      shortcut: 'm',
+      condition: (composer) => {
+        return composer.editingPost;
+      }
+    });
+    
+    api.addUsernameSelectorDecorator(username => {
+      return `<span class="status">[is_away]</class>`;
+    });
+});
